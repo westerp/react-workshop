40baff9a88ad12a44c98dfd28f47f0b7a8e1354d
Rudfoss <Thomas.Rudfoss@dnvgl.com>
2018-04-17 13:49:56 +0200

    Improved nav design

--- src/nav/NavContainer.js
+++ src/nav/NavContainer.js
@@ -1,15 +1,28 @@
 import React from 'react';
-
-import {NavLink} from 'react-router-dom';
+import {NavLink, withRouter} from 'react-router-dom';
 
 import classes from './NavContainer.scss';
 
-export const NavContainer = () => (
-  <nav>
-    <ol className={classes.menu}>
-      <li><NavLink to="/" exact>Home</NavLink></li>
-      <li><NavLink to="/basics">Basics</NavLink></li>
-    </ol>
-  </nav>
-);
-export default NavContainer;
+export class NavContainer extends React.PureComponent{
+  state = {
+    menuOpen: false
+  }
+
+  toggleMenuOpen = () => {
+    this.setState({
+      menuOpen: !this.state.menuOpen
+    });
+  }
+
+  render(){
+    return (
+      <nav>
+        <ol className={classes.menu}>
+          <li><NavLink to="/" exact>Home</NavLink></li>
+          <li><NavLink to="/basics">Basics</NavLink></li>
+        </ol>
+      </nav>
+    );
+  }
+}
+export default withRouter(NavContainer);
