d4998a38397134a9f7b76d0f8bb114a1ba154dbb
Rudfoss <Thomas.Rudfoss@dnvgl.com>
2018-04-17 07:35:58 +0200

    Start here

--- src/basics/BasicsContainer.js
+++ src/basics/BasicsContainer.js
@@ -1,23 +1,11 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import Button from './Button';
-import Form from './Form';
-
 export class BasicContainer extends Component{
 	render(){
 		return (
 			<div>
-				<ol>
-					<li>Create a basic component with a Button that responds to clicks</li>
-					<li>Create a basic form with a text input and submit that posts the form data to console</li>
-				</ol>
-				<div>
-					<Button>Example button</Button>
-				</div>
-				<div>
-					<Form/>
-				</div>
+				Nothing to see here... move along
 			</div>
 		);
 	}
