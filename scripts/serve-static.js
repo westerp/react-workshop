007969178dc930f14f3c441a5b527a39585d8213
Thomas <thomas.rudfoss@gmail.com>
2018-09-18 17:37:07 +0200

    Added logging to serve-static

--- scripts/serve-static.js
+++ scripts/serve-static.js
@@ -4,5 +4,10 @@ const app = express()
 
 const root = path.resolve(__dirname, "../dist")
 
+const port = 8080
+
 app.use(express.static(root))
-app.listen(8080)
+app.listen(port)
+
+/* eslint no-console: 0 */
+console.log(`Serving @ localhost:${port}. Ctrl+C to kill`)
