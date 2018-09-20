5886692b178285d7dc2d5bf9a99942228b8c696e
Thomas <thomas.rudfoss@gmail.com>
2018-09-20 13:25:59 +0200

    Added devtools for redux

--- src/ducks/index.duck.js
+++ src/ducks/index.duck.js
@@ -1,9 +1,9 @@
 import {createAction, handleActions} from "redux-actions"
 
-export const myFirstAction = createAction("MY_FIRST_ACTION")
+export const myFirstAction = createAction("MY_FIRST_ACTION", (id) => (id))
 
 export const reducer = handleActions({
-	["MY_FIRST_ACTION"]: (state, action) => {
+	[myFirstAction]: (state, action) => {
 		return {
 			...state,
 			counter: (state.counter || 0) + 1
--- src/store/createStore.development.js
+++ src/store/createStore.development.js
@@ -1,9 +1,12 @@
-import {createStore, applyMiddleware, combineReducers} from "redux"
+import {createStore, applyMiddleware, combineReducers, compose} from "redux"
+const composer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 import reducer from "../ducks/index.duck"
 
 export const createReduxStore = (initialState = {}) => {
 	return createStore(
 		reducer,
-		initialState
+		initialState,
+		composer()
 	)
 }
