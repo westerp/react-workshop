5d89061551ae171b8b8ba987716f3f388543b9fb
Thomas <thomas.rudfoss@gmail.com>
2018-11-02 13:00:02 +0100

    Added users duck to state

---
+++ src/ducks/users.duck.js
@@ -0,0 +1,27 @@
+import {createAction, handleActions} from "redux-actions"
+
+export const getState = (globalState) => globalState.users || {}
+
+export const getUser = (globalState, id) => {
+	return (getState(globalState).byId || {})[id]
+}
+export const getUsers = (globalState) => {
+	const state = getState(globalState)
+	return (state.order || []).map((id) => (state.byId || {})[id])
+}
+
+export const addUser = createAction("ADD_USER")
+
+export const reducer = handleActions({
+	[addUser]: (state, action) => {
+		return {
+			...state,
+			order: state.order.concat([action.payload.id]),
+			byId: {
+				...(state.byId || {}),
+				[action.payload.id]: action.payload
+			}
+		}
+	}
+}, {})
+export default reducer
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,7 +1,9 @@
 import {combineReducers} from "redux"
 
 import newUser from "../ducks/newUser.duck"
+import users from "../ducks/users.duck"
 
 export default combineReducers({
-	newUser
+	newUser,
+	users
 })
