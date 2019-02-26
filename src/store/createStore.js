0fa839b305762c4c03f0a8756d343e15a480d782
Thomas <thomas.rudfoss@gmail.com>
2019-02-26 21:10:05 +0100

    Upgraded connected-react-router

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -2,7 +2,7 @@ import {createStore as createReduxStore, applyMiddleware, compose} from "redux"
 import thunk from "redux-thunk"
 
 import createBrowserHistory from "history/createBrowserHistory"
-import { connectRouter, routerMiddleware } from "connected-react-router"
+import {routerMiddleware} from "connected-react-router"
 
 import rootReducer from "./reducers"
 
@@ -10,7 +10,7 @@ export const history = createBrowserHistory()
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const createStore = initialState => createReduxStore(
-	connectRouter(history)(rootReducer),
+	rootReducer(history),
 	initialState,
 	composeEnhancers(
 		applyMiddleware(thunk, routerMiddleware(history))
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,6 +1,8 @@
 import {combineReducers} from "redux"
+import {connectRouter} from "connected-react-router"
 import reducers from "../ducks"
 
-export default combineReducers({
+export default (history) => combineReducers({
+	router: connectRouter(history),
 	...reducers
 })
